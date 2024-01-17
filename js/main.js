15ce4ab85b3e61991e4e257e7749bfa6c5516cd7
AndrejVenas <84439808+AndrejVenas@users.noreply.github.com>
2024-01-17 23:29:46 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,17 @@
-var mixer = mixitup('.gallery__box');
+$('.menu__btn').click(function(){
+  $('.menu__list').toggleClass('menu__list--active');
+});
 $('.blog__slider').slick({
   arrows: false,
   dots: true
 });
+
+var largeImg = document.getElementsByClassName('large-img');
+const screenWidth = window.screen.width;
+if(screenWidth < 641){
+  for (var element of largeImg) {
+    element.classList.remove('mix');
+}
+}
+
+var mixer = mixitup('.gallery__box');
