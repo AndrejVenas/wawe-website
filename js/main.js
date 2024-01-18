3ae034a21d909669c515b3424ad3fe26e8045e98
AndrejVenas <84439808+AndrejVenas@users.noreply.github.com>
2024-01-18 23:13:01 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,28 @@
 $('.menu__btn').click(function(){
   $('.menu__list').toggleClass('menu__list--active');
+  $('.menu__btn').toggleClass('menu__btn--active');
 });
+
+$(".menu a").on("click", function(e) {
+  e.preventDefault();
+  var id = $(this).attr('href'),
+      top = $(id).offset().top;
+  $('body, html').animate({scrollTop: top}, 1500);
+  $('.menu__list').removeClass('menu__list--active');
+  $('.menu__btn').removeClass('menu__btn--active');
+});
+
+let header = document.querySelector(".header")
+window.onscroll = function()
+{
+  if(window.scrollY > 100){
+    header.style.background = "#063740"
+  }
+  else{
+    header.style.background = "rgba(0, 0, 0, 0)" 
+  }
+}
+
 $('.blog__slider').slick({
   arrows: false,
   dots: true
